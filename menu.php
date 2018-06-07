5404b40f6756615bec7ddd940f36a38f901171a0
LilyeShire <40024862+LilyeShire@users.noreply.github.com>
2018-06-08 00:16:48 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="css_menu.css" />
+        <title>Menu</title>
+    </head>
+
+    <body>
+		<nav>
+			<p class="menu">MENU</p>
+			<p class="menu">Marie Curie et ses découvertes</p>
+			<p class="menu">Une histoire, un projet</p>
+			<p class="menu">Impact sur les cellules</p>
+			<p class="menu">Maladies radio-induites</p>
+			<p class="menu">Radioprotection</p>
+		</nav>
+    </body>
+</html>
---
+++ page1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="css_page1.css" />
+        <title>Introduction</title>
+    </head>
+
+    <body>
+		<?php include ("menu.php") ?>
+		<p>paragraphe</p>
+    </body>
+</html>
